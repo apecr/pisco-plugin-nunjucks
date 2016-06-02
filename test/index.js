bd056d47a56e431c9520ff48226439c12db2c0ce
Felipe Roucher Iglesias <felipe.roucher@bbva.com>
2016-06-02 09:00:35 +0200

    test(index.js): fixing a possible sync error with "Promise"

--- test/index.js
+++ test/index.js
@@ -28,48 +28,57 @@ describe('Pisco plugin nunjucks test:', function() {
     let fromFile = './test/test1/from/test1.njk';
     let toFile = './test/test1/to/test1.njk';
 
-    //Creating the templates files
-    fs.writeFile(fromFile, 'one, {{ two }}, three', function(err) {
-      chai.assert.isNull(err, `file '${fromFile}' is not been written`);
-    });
+    //Initialize Test Environment
+    let initTestEnv = new Promise((resolve, reject) => {
+      fs.writeFile(fromFile, 'one, {{ two }}, three', function(err) {
+        chai.assert.isNull(err, `file '${fromFile}' is not been written`);
 
-    fs.writeFile(toFile, 'Hey there!', function(err) {
-      chai.assert.isNull(err, `file '${toFile}' is not been written`);
+        fs.writeFile(toFile, 'Hey there!', function(err) {
+          chai.assert.isNull(err, `file '${toFile}' is not been written`);
+
+	  resolve();
+        });
+      });
     });
 
     //plugin njkRender tested:
-    plugin.addons.njkRender(path.resolve('./test/test1'), 'from', 'to', { two: 'four' })
-      .then(() => {
-        fs.readFile(toFile, 'utf8', function (err, data) {
-          chai.assert.isNull(err, `file '${toFile}' does not open`);
-          chai.assert.equal(data, 'one, four, three', `nunjucks file '${toFile}' render fails`);
-          done();
+    initTestEnv.then(
+      plugin.addons.njkRender(path.resolve('./test/test1'), 'from', 'to', { two: 'four' })
+        .then(() => {
+          fs.readFile(toFile, 'utf8', function (err, data) {
+            chai.assert.isNull(err, `file '${toFile}' does not open`);
+            chai.assert.equal(data, 'one, four, three', `nunjucks file '${toFile}' render fails`);
+            done();
+          })
         })
-      })
-      .catch((reason) => {
-        chai.assert.fail(false, reason);
-      });
+        .catch((reason) => {
+          chai.assert.fail(false, reason);
+        }));
   });
 
   it('Should render nunjunks templates in the same path', function(done) {
     let fromtoFile = './test/test2/fromto/test2.njk';
 
-    //Creating the templates files
-    fs.writeFile(fromtoFile, 'one,{% for item in items %}{{ item.title }},{% endfor %}four', function(err) {
-      chai.assert.isNull(err, `file '${fromtoFile}' is not been written`);
+    //Initialize Test Environment
+    let initTestEnv = new Promise((resolve, reject) => {
+      fs.writeFile(fromtoFile, 'one,{% for item in items %}{{ item.title }},{% endfor %}four', function(err) {
+        chai.assert.isNull(err, `file '${fromtoFile}' is not been written`);
+        resolve();
+      });
     });
 
     //plugin njkRender tested:
-    plugin.addons.njkRender(path.resolve('./test'), 'test2/fromto', 'test2/fromto', {items: [{title: 'two'}, {title: 'three'}]})
-      .then(() => {
-        fs.readFile(fromtoFile, 'utf8', function (err, data) {
-          chai.assert.isNull(err, `file '${fromtoFile}' does not open`);
-          chai.assert.equal(data, 'one,two,three,four', `nunjucks file '${fromtoFile}' render fails`);
-          done();
+    initTestEnv.then(
+      plugin.addons.njkRender(path.resolve('./test'), 'test2/fromto', 'test2/fromto', {items: [{title: 'two'}, {title: 'three'}]})
+        .then(() => {
+          fs.readFile(fromtoFile, 'utf8', function (err, data) {
+            chai.assert.isNull(err, `file '${fromtoFile}' does not open`);
+            chai.assert.equal(data, 'one,two,three,four', `nunjucks file '${fromtoFile}' render fails`);
+            done();
+          })
         })
-      })
-      .catch((reason) => {
-        chai.assert.fail(false, reason);
-      });
+        .catch((reason) => {
+          chai.assert.fail(false, reason);
+        }));
   });
 });
